0b57bca6abf97c97b63d7c8140041f42144ac756
John Krauss <irving.krauss@gmail.com>
2015-02-07 00:43:14 -0500

    fixes to queries

--- images/nyc-acris/queries/boa-mortgages.sql
+++ images/nyc-acris/queries/boa-mortgages.sql
@@ -3,10 +3,12 @@
  * name of the bank from 'BANK OF AMERICA' to anyone you'd like.
  */
 
+/* CREATE INDEX ON acris_parties (name); */
+
 SELECT DISTINCT lonlat
 FROM acris_master, acris_legals, acris_parties, pluto
 WHERE doc_type = 'MTGE'
-  AND name ILIKE 'BANK OF AMERICA'
+  AND name LIKE 'BANK OF AMERICA'
   AND party_type = 2
   AND DATE_PART('year', document_date) = 2014;
 
