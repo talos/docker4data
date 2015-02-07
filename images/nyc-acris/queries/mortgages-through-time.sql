c722a6442ff299d92952de81961e541248d7cf08
john krauss <talos@users.noreply.github.com>
2015-02-07 12:29:03 -0500

    Update mortgages-through-time.sql
    Fixing spaces.

--- images/nyc-acris/queries/mortgages-through-time.sql
+++ images/nyc-acris/queries/mortgages-through-time.sql
@@ -6,8 +6,8 @@
 SELECT
   COUNT(DISTINCT document_id) AS cnt,
   DATE_PART('year', recorded_datetime) AS year
-FROM acris_master
-WHERE doc_type = 'MTGE'
-  AND recorded_datetime > '2000-01-01'
-GROUP BY year
-ORDER BY year;
+  FROM acris_master
+  WHERE doc_type = 'MTGE'
+    AND recorded_datetime > '2000-01-01'
+  GROUP BY year
+  ORDER BY year;
