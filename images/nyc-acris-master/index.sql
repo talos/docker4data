acc1ea8f595cc85e525100781d190922253f1979
John Krauss <irving.krauss@gmail.com>
2015-02-23 13:45:56 -0500

    removing individual images

--- images/nyc-acris-master/index.sql
+++
@@ -1,3 +0,0 @@
-BEGIN TRANSACTION;
-ALTER TABLE acris_master ADD PRIMARY KEY ("document_id", "good_through_date");
-END TRANSACTION;
--- images/nyc-acris-master/wait.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo foo
-
-while true;
-do
-  sleep 1
-done
