09f5ef5c1a9916e0361b54a5a6275cbbe6015e28
John Krauss <irving.krauss@gmail.com>
2015-02-23 16:49:46 -0500

    use correct acris_master URL

---
+++ data/nyc_acris_parties/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "nyc_acris_legals" (
+	"document_id"	text,
+	"record_type"	text,
+	"borough"	smallint,
+	"block"	integer,
+	"lot"	integer,
+	"easement"	text,
+	"partial_lot"	text,
+	"air_rights"	text,
+	"subterranean_rights"	text,
+	"property_type"	text,
+	"street_number"	text,
+	"street_name"	text,
+	"addr_unit"	text,
+	"good_through_date"	text
+	--, FOREIGN KEY ("Document ID", "Good_through date")
+	--	REFERENCES "acris_master" ("Document ID", "Good_through date")
+);
