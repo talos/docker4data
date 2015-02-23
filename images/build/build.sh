20a2fcc2d64989edb9cba4021849b0110e1ad4d5
John Krauss <irving.krauss@gmail.com>
2015-02-23 13:37:49 -0500

    rename buildbase to build

--- images/build/build.sh
+++ images/build/build.sh
@@ -2,13 +2,23 @@
 
 PWD=$(pwd)
 
-# First, load up a psql ready docker4data linked up to a dietfs export data container
-BUILD_CONTAINER=$(docker run -d -v ${PWD}:/csv thegovlab/docker4data-buildbase /scripts/postgres.sh)
+# First, load up a psql ready docker4data-buildbase to import data
+BUILD_CONTAINER=$(docker run -d -v ${PWD}:/share thegovlab/docker4data-buildbase /scripts/postgres.sh)
 
-sleep 1
+#sleep 1
 
-docker exec ${BUILD_CONTAINER} ls -la /csv
+# Import the csv using the supplied schema
 docker exec ${BUILD_CONTAINER} /bin/bash /scripts/load.sh -u -s ',' acris_master
+
+# Dump it to the dietfs export image, and save that as a data image
+docker exec ${BUILD_CONTAINER} chown postgres:postgres /share
+docker exec ${BUILD_CONTAINER} gosu postgres pg_dump -F c -Z 9 -t acris_master -f /share/dump postgres
+
+docker build -t thegovlab/docker4data-nyc-acris-master .
+
+# to be used down the line...
+#DATA_CONTAINER=$(docker run -v /share ${DATA_IMAGE})
+#time gosu postgres pg_restore -d postgres /share/dump > log 2>error
 
 #BUILD_IMAGE=$(docker commit ${BUILD_CONTAINER})
 #echo build image: ${BUILD_IMAGE}
@@ -34,6 +44,4 @@ docker exec ${BUILD_CONTAINER} /bin/bash /scripts/load.sh -u -s ',' acris_master
 #docker rm -f ${DATA_CONTAINER}
 #docker rm -f ${BUILD_CONTAINER}
 
-# Import the csv using the supplied schema
 
-# Dump it to the dietfs export image, and save that as a data image
