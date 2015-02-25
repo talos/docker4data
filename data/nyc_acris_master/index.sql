fc8f7636ae464db15d20e0d6637753718275792f
John Krauss <irving.krauss@gmail.com>
2015-02-25 10:48:26 -0500

    control for bad good_through_date

--- data/nyc_acris_master/index.sql
+++ data/nyc_acris_master/index.sql
@@ -1,3 +1,4 @@
 BEGIN TRANSACTION;
+DELETE FROM nyc_acris_master WHERE "good_through_date" IS NULL;
 ALTER TABLE nyc_acris_master ADD PRIMARY KEY ("document_id", "good_through_date");
 END TRANSACTION;
