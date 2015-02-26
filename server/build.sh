8f54b51414a612e9193288615b6a1d4f97a4d744
John Krauss <irving.krauss@gmail.com>
2015-02-26 15:52:54 -0500

    use actual tmp directory, fail-fast build, limit length of tag

--- server/build.sh
+++ server/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ ! $1 ]; then
   echo 'Must specify URL of data.json to build as first argument.
@@ -9,29 +9,36 @@ if [ ! $1 ]; then
   exit 1
 fi
 
-#docker build -t thegovlab/docker4data-build .
-
 PWD=$(pwd)
-mkdir -p share
+TMPDIR=$(mktemp -d /tmp/docker4data-build.XXXX)
 
 # First, load up a psql ready docker4data-build to download and import data
-BUILD_CONTAINER=$(docker run -d -v ${PWD}/share:/share thegovlab/docker4data-build /scripts/postgres.sh)
+BUILD_CONTAINER=$(docker run -d -v ${PWD}/$TMPDIR:/share thegovlab/docker4data-build /scripts/postgres.sh)
 
 # Import the csv using the supplied schema
-echo $1
 docker exec ${BUILD_CONTAINER} python /scripts/build.py $1
-#docker exec ${BUILD_CONTAINER} /bin/bash /scripts/load.sh
+
+NAME=$(docker exec ${BUILD_CONTAINER} cat /name)
+echo $NAME
 
 # Dump it to the dietfs export image, and save that as a data image
 docker exec ${BUILD_CONTAINER} chown postgres:postgres /share
-docker exec ${BUILD_CONTAINER} /bin/bash /scripts/dump.sh
-NAME=$(docker exec ${BUILD_CONTAINER} cat /name)
+docker exec ${BUILD_CONTAINER} /bin/bash /scripts/dump.sh $NAME
 
-mkdir -p tmp
+mkdir -p tmp  # because tmp dockerfile must be in build context
 TMP_DOCKERFILE=tmp/Dockerfile-${NAME}
-cat "Dockerfile" | sed "s/dataset/${NAME}/g" > ${TMP_DOCKERFILE}
-docker build -t thegovlab/docker4data-${NAME} -f ${TMP_DOCKERFILE} .
-docker push thegovlab/docker4data-${NAME}
+cat "Dockerfile" | \
+  sed "s!dataset!${NAME}!g" | \
+  sed "s!share_path!${TMPDIR}!g" \
+  > ${TMP_DOCKERFILE}
+
+# Ensure name is not too long for docker hub
+TAG=thegovlab/d4d-$(echo ${NAME} | cut -c 1-26)
+
+docker build -t ${TAG} -f ${TMP_DOCKERFILE} .
+rm -rf $TMPDIR
+
+docker push ${TAG}
 
 
 # to be used down the line...
