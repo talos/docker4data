72426f42dd10646121a918bf7a6dde7d14990a8b
talos <irving.krauss@gmail.com>
2015-02-27 17:24:48 -0500

    include nyc_pluto in server.sh mockup

--- server/server.sh
+++ server/server.sh
@@ -3,4 +3,5 @@
 ./build.sh 'https://raw.githubusercontent.com/talos/docker4data/master/data/nyc_acris_legals/data.json' > legals.log 2>&1 &
 ./build.sh 'https://raw.githubusercontent.com/talos/docker4data/master/data/nyc_acris_master/data.json' > master.log 2>&1 &
 ./build.sh 'https://raw.githubusercontent.com/talos/docker4data/master/data/nyc_acris_parties/data.json' > parties.log 2>&1 &
+./build.sh 'https://raw.githubusercontent.com/talos/docker4data/master/data/nyc_pluto/data.json' > nyc_pluto.log 2>&1 &
 ./build.sh 'https://raw.githubusercontent.com/talos/docker4data/master/data/nyc_multiple_dwelling_registrations/data.json' > multiple_dwelling_registrations.log 2>&1 &
