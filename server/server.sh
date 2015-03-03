fce0168506693084bfc6615e036e71138429caea
John Krauss <irving.krauss@gmail.com>
2015-03-03 14:28:33 -0500

    additional pluto in server

--- server/server.sh
+++ server/server.sh
@@ -5,3 +5,4 @@
 ./build.sh 'https://raw.githubusercontent.com/talos/docker4data/master/data/nyc_acris_parties/data.json' > parties.log 2>&1 &
 ./build.sh 'https://raw.githubusercontent.com/talos/docker4data/master/data/nyc_pluto/data.json' > nyc_pluto.log 2>&1 &
 ./build.sh 'https://raw.githubusercontent.com/talos/docker4data/master/data/nyc_multiple_dwelling_registrations/data.json' > multiple_dwelling_registrations.log 2>&1 &
+./build.sh 'https://raw.githubusercontent.com/talos/docker4data/master/data/nyc_pluto/data.json' > pluto.log 2>&1 &
