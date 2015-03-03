4bebd0d79984fde025eb461d579556b40819c973
John Krauss <irving.krauss@gmail.com>
2015-03-03 17:47:28 -0500

    improvements to visual image

--- images/visual-splunk/build.sh
+++ images/visual-splunk/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker build -t thegovlab/docker4data-vis-splunk:latest .
+docker push thegovlab/docker4data-vis-splunk:latest
--- images/visual-splunk/configure.sh
+++ images/visual-splunk/configure.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+touch /opt/splunk/etc/.ui_login
+
+#cat app.conf
+echo '[install]
+is_configured = 1' > /opt/splunk/etc/apps/dbx/local/app.conf
+
+#cat java.conf
+echo '[java]
+home = /usr/lib/jvm/java-7-openjdk-amd64' > /opt/splunk/etc/apps/dbx/local/java.conf
+
+#cat inputs.conf
+echo '[script://./bin/jbridge_server.py]
+disabled = 0
+
+[batch://$SPLUNK_HOME/var/spool/dbmon/*.dbmonevt]
+crcSalt = <SOURCE>
+disabled = 0
+move_policy = sinkhole
+sourcetype = dbmon:spool' > /opt/splunk/etc/apps/dbx/local/inputs.conf
+
+#cat local.meta
+echo '[database/docker4data]
+version = 6.2.1
+modtime = 1425421294.054959000' > /opt/splunk/etc/apps/dbx/metadata/local.meta
+
 echo "host = $DOCKER4DATA_PORT_5432_TCP_ADDR" >> /opt/splunk/etc/apps/dbx/local/database.conf
 echo "port = $DOCKER4DATA_PORT_5432_TCP_PORT" >> /opt/splunk/etc/apps/dbx/local/database.conf
 echo "password = $DOCKER4DATA_ENV_PASSWORD" >> /opt/splunk/etc/apps/dbx/local/database.conf
