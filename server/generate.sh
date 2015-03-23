dac391064c96b5d5503dcc92a1ae2c19ff429182
John Krauss <irving.krauss@gmail.com>
2015-03-23 17:24:06 -0400

    use opendatacaches own log listings

--- server/generate.sh
+++ server/generate.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -e
 
-PORTALS_URL=https://raw.githubusercontent.com/talos/opendatacache/master/site/portals.txt
+PORTALS_URL=http://www.opendatacache.com/logs/status.log
 
-for portal in $(curl -k -s -S ${PORTALS_URL}); do
-  portal_url=http://www.opendatacache.com/$portal/data.json
-  for id in $(curl -k -s -S $portal_url | grep -Po '"identifier":(.*?[^\\])",' | grep -Po '[\d\w]{4}-[\d\w]{4}'); do
-    if [ $id == "data.json" ]; then
-      continue
-    fi
+for portal in $(curl -k -s -S ${PORTALS_URL} | cut -f 1); do
+  portal_url=http://www.opendatacache.com/logs/$portal/summary.log
+  echo $portal_url
+  for id in $(curl -k -s -S $portal_url | grep -P '^200|^201' | cut -f 2); do
     dataset_url=http://www.opendatacache.com/${portal}/api/views/${id}.json
-    echo $dataset_url
     python infer.py $dataset_url ../data
   done
 done
