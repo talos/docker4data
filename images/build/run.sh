627027d7124b2f6d96218b0f5b9d800a94700f1c
John Krauss <irving.krauss@gmail.com>
2015-06-17 15:56:05 -0400

    Improvements to socrata inference
    * Allow for inference of single portal
    * Use host machine's git for easier pushing

--- images/build/run.sh
+++ images/build/run.sh
@@ -5,4 +5,5 @@ docker run -d --name docker4data-build \
   -v $(pwd)/keys/.ssh:/root/.ssh \
   -v $(pwd)/keys/.aws:/root/.aws \
   -v $(pwd)/scripts:/scripts \
+  -v $(pwd)/../../data:/docker4data/data \
   thegovlab/docker4data-build /scripts/postgres.sh
--- images/build/scripts/socrata-generate-metadata.sh
+++ images/build/scripts/socrata-generate-metadata.sh
@@ -1,31 +1,23 @@
 #!/bin/bash -e
 
-### Regenerate JSON in git repo from socrata data portals
+### Regenerate JSON in git repo from socrata all socrata data portals, or only
+### the one specified if one is specified
 
 PORTALS_URL=http://www.opendatacache.com/logs/status.log
 
-mkdir -p /git
-if [ -e /git/docker4data ]; then
-  cd /git/docker4data
-  git fetch origin
-  git clean -f
-  git checkout -- .
-  git checkout origin/master
-  git checkout -B master
-else
-  cd /git
-  git clone git@github.com:talos/docker4data
-fi
-
 function generate_portal {
-  portal_url=$1
-  echo $portal_url
+  portal=$1
+  portal_url=http://www.opendatacache.com/logs/$portal/summary.log
   for id in $(wget -O - -q $portal_url | grep -P '^200|^201' | cut -f 2); do
     dataset_url=http://www.opendatacache.com/${portal}/api/views/${id}.json
-    python /scripts/infer.py $dataset_url /git/docker4data/data
+    python /scripts/infer.py $dataset_url /docker4data/data
   done
 }
 
-for portal in $(wget -O - -q ${PORTALS_URL} | cut -f 1); do
-  generate_portal http://www.opendatacache.com/logs/$portal/summary.log
-done
+if [ $1 ]; then
+  generate_portal $1
+else
+  for portal in $(wget -O - -q ${PORTALS_URL} | cut -f 1); do
+    generate_portal $portal
+  done
+fi
