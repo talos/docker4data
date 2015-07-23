710860dd65006c523f3eb8796e1b2b35d50d5e39
John Krauss <irving.krauss@gmail.com>
2015-07-23 10:00:24 -0400

    removing erroneous `head`

--- images/base/cli/ls.sh
+++ images/base/cli/ls.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-find /docker4data/data/ -iname data.json | head | sed s:/docker4data/data/:: | sed s:/data.json::
+find /docker4data/data/ -iname data.json | sed s:/docker4data/data/:: | sed s:/data.json::
