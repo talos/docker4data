e74c8ed5b6251bccb50679272658c4b68413712f
John Krauss <irving.krauss@gmail.com>
2015-07-24 17:42:55 -0400

    temporary sales work

--- data/contrib/us/ny/nyc/sales/after.sql
+++ data/contrib/us/ny/nyc/sales/after.sql
@@ -1,4 +1,4 @@
-CREATE VIEW "contrib/us/ny/nyc".acris_real
+CREATE TABLE sales
 AS
 SELECT * FROM "socrata/data.cityofnewyork.us".acris_real_property_master,
               "socrata/data.cityofnewyork.us".acris_real_property_legals,
