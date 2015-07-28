0fb89f34ee288343f25cfcb7a17d2f122683ffc4
John Krauss <irving.krauss@gmail.com>
2015-07-28 16:45:35 -0400

    create useful deeds table

--- data/contrib/us/ny/nyc/sales/after.sql
+++ data/contrib/us/ny/nyc/sales/after.sql
@@ -1,5 +1,77 @@
-CREATE TABLE sales
+CREATE INDEX master_idx ON "socrata/data.cityofnewyork.us".acris_real_property_master (document_id, good_through_date);
+CREATE INDEX legals_idx ON "socrata/data.cityofnewyork.us".acris_real_property_legals (document_id, good_through_date);
+CREATE INDEX parties_idx ON "socrata/data.cityofnewyork.us".acris_real_property_parties (document_id, good_through_date);
+
+CREATE TABLE deeds_master
 AS
-SELECT * FROM "socrata/data.cityofnewyork.us".acris_real_property_master,
-              "socrata/data.cityofnewyork.us".acris_real_property_legals,
-              "socrata/data.cityofnewyork.us".acris_real_property_parties;
+SELECT DISTINCT (
+       CASE substr(document_id, 0, 3)
+          WHEN '20' THEN document_id
+          WHEN 'FT' THEN '100' || substr(document_id, 4)
+          WHEN 'BK' THEN '000' || substr(document_id, 4)
+          ELSE document_ID END)::BIGINT as document_id,
+       m.good_through_date, m.document_date,
+       m.document_amt, m.recorded_datetime, m.modified_date,
+       dcc.doc__type_description, dcc.doc__type as doc_type
+ FROM "socrata/data.cityofnewyork.us".acris_real_property_master m,
+      "socrata/data.cityofnewyork.us".acris_document_control_codes dcc
+WHERE dcc.class_code_description = 'DEEDS AND OTHER CONVEYANCES' AND
+      dcc.doc__type = m.doc_type
+;
+
+DELETE FROM deeds_master USING deeds_master alias
+  WHERE deeds_master.document_id = alias.document_id AND
+    deeds_master.good_through_date < alias.good_through_date;
+
+CREATE UNIQUE INDEX deeds_master_docid ON deeds_master (document_id);
+
+CREATE TABLE deeds_parties
+AS
+SELECT DISTINCT m.document_id, p.good_through_date,
+      CASE p.party_type WHEN '1' THEN dcc.party1_type
+                        WHEN '2' THEN dcc.party2_type
+                        WHEN '3' THEN dcc.party3_type
+      ELSE p.party_type END AS party_type,
+      p.name, p.addr1, p.addr2, p.country, p.city, p.state, p.zip
+FROM "socrata/data.cityofnewyork.us".acris_real_property_parties p,
+     deeds_master m,
+     "socrata/data.cityofnewyork.us".acris_document_control_codes dcc
+WHERE (CASE substr(p.document_id, 0, 3)
+          WHEN '20' THEN p.document_id
+          WHEN 'FT' THEN '100' || substr(p.document_id, 4)
+          WHEN 'BK' THEN '000' || substr(p.document_id, 4)
+          ELSE p.document_id END)::BIGINT = m.document_id AND
+      m.good_through_date = p.good_through_date AND
+      SUBSTR(p.document_id, 4) ~ '^[0-9]+$' AND
+      dcc.doc__type = m.doc_type;
+
+CREATE INDEX deeds_parties_docid ON deeds_parties (document_id);
+
+CREATE TABLE deeds_legals
+AS
+SELECT DISTINCT m.document_id, m.good_through_date,
+       (borough * 1000000000) + (block * 10000) + lot as bbl,
+       l.easement, l.partial_lot, l.air_rights, l.subterranean_rights,
+       l.property_type, l.addr_unit
+FROM "socrata/data.cityofnewyork.us".acris_real_property_legals l,
+     deeds_master m WHERE (CASE substr(l.document_id, 0, 3)
+          WHEN '20' THEN l.document_id
+          WHEN 'FT' THEN '100' || substr(l.document_id, 4)
+          WHEN 'BK' THEN '000' || substr(l.document_id, 4)
+          ELSE l.document_id END)::BIGINT = m.document_id AND
+      m.good_through_date = l.good_through_date;
+
+CREATE INDEX deeds_legals_docid ON deeds_legals (document_id);
+
+CREATE VIEW deeds AS
+SELECT m.*, l.easement, l.partial_lot, l.air_rights, l.subterranean_rights,
+       l.property_type, l.addr_unit, party_type,
+      p.name, p.addr1, p.addr2, p.country, p.city, p.state, p.zip, pluto.*
+FROM deeds_legals l, deeds_master m, deeds_parties p, pluto
+WHERE
+l.document_id = m.document_id AND m.document_id = p.document_id AND
+l.bbl = pluto.bbl;
+
+DROP TABLE "socrata/data.cityofnewyork.us".acris_real_property_master;
+DROP TABLE "socrata/data.cityofnewyork.us".acris_real_property_legals;
+DROP TABLE "socrata/data.cityofnewyork.us".acris_real_property_parties;
