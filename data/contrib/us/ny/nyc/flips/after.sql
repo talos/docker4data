d322d9c3591cf3dde3f0f78b1c0bcdfb7a81d370
John Krauss <irving.krauss@gmail.com>
2015-08-11 09:09:18 -0400

    use deeds for flips

--- data/contrib/us/ny/nyc/flips/after.sql
+++ data/contrib/us/ny/nyc/flips/after.sql
@@ -1,29 +1,24 @@
-SELECT l.*,
-  m.crfn, m.recorded_borough, m.doc_type, m.document_date, m.document_amt, m.recorded_datetime, m.modified_date, m.percent_trans, p.party_type, p.name, p.addr1, p.addr2, p.country, p.city, p.state, p.zip
+SELECT *
 INTO deriv
-FROM acris_real_property_legals l, acris_real_property_master m, acris_real_property_parties p
-WHERE l.document_id = m.document_id
-  AND m.document_id = p.document_id
-  AND m.recorded_datetime >= '2003-01-01'
+FROM deeds
+  WHERE recorded_datetime >= '2003-01-01'
   AND doc_type in ('DEED', 'DEEDO')
+  AND document_amt > 100000
 ;
 
 CREATE INDEX document_id on deriv (document_id);
-CREATE INDEX bbl ON deriv (borough, block, lot);
+CREATE INDEX bbl ON deriv (bbl);
 
-SELECT document_id, count(distinct borough || '|' || block || '|' || lot) INTO acris_real_property_dupes FROM deriv GROUP BY document_id;
+SELECT document_id, count(distinct bbl) INTO acris_real_property_dupes FROM deriv GROUP BY document_id;
 CREATE INDEX document_id_key on acris_real_property_dupes (document_id);
 
 DELETE FROM deriv
 WHERE document_id IN
   (SELECT document_id FROM acris_real_property_dupes WHERE "count" > 1);
-DELETE FROM deriv WHERE document_amt < 100000;
 
 SELECT
   before.document_id before_document_id,
-  before.borough borough,
-  before.block block,
-  before.lot lot,
+  before.bbl bbl,
   before.document_date before_document_date,
   before.document_amt before_document_amt,
   before.recorded_datetime before_recorded_datetime,
@@ -46,18 +41,17 @@ SELECT
   after.country after_country,
   after.city after_city,
   after.state after_state,
-  after.zip after_zip
+  after.zip after_zip,
+  after.cd, after.ct2010, after.cb2010, after.council, after.zipcode,
+  after.address, after.unitsres, after.unitstotal, after.yearbuilt,
+  after.condono, after.geom
 INTO flips_raw
 FROM deriv before, deriv after
 WHERE after.document_date - before.document_date BETWEEN 1 and 730
   AND before.document_amt * 1.5 < after.document_amt
-  AND before.borough = after.borough
-  AND before.block = after.block
-  AND before.lot = after.lot;
+  AND before.bbl = after.bbl;
 
-SELECT borough,
-  block,
-  lot,
+SELECT bbl,
   MAX(before_document_date) before_document_date,
   MAX(after_document_date) after_document_date,
   MAX(before_document_amt) before_document_amt,
@@ -67,23 +61,18 @@ SELECT borough,
   SUBSTR(STRING_AGG(CASE WHEN before_party_type = 1 THEN before_name ELSE NULL END, '|'), 0, 255) AS sellers_before,
   SUBSTR(STRING_AGG(CASE WHEN before_party_type = 2 THEN before_name ELSE NULL END, '|'), 0, 255) AS buyers_before,
   SUBSTR(STRING_AGG(CASE WHEN after_party_type = 1 THEN after_name ELSE NULL END, '|'), 0, 255) AS sellers_after,
-  SUBSTR(STRING_AGG(CASE WHEN after_party_type = 2 THEN after_name ELSE NULL END, '|'), 0, 255) AS buyers_after
-INTO flips_output
+  SUBSTR(STRING_AGG(CASE WHEN after_party_type = 2 THEN after_name ELSE NULL END, '|'), 0, 255) AS buyers_after,
+  MAX(after.cd) cd,
+  MAX(after.ct2010) ct2010,
+  MAX(after.cb2010) cb2010,
+  MAX(after.council) council,
+  MAX(after.zipcode) zipcode,
+  MAX(after.address) address,
+  MAX(after.unitsres) unitsres,
+  MAX(after.unitstotal) unitstotal,
+  MAX(after.yearbuilt) yearbuilt,
+  MAX(after.condono) condono,
+  MAX(after.geom) geom
+INTO flips
 FROM flips_raw
-GROUP BY borough, block, lot;
-
--- to join the queried acris_real_property data to pluto:
-CREATE TABLE flips AS
-SELECT a.*, 
-  b.before_document_date,
-  b.after_document_date,
-  b.before_document_amt,
-  b.after_document_amt,
-  b.sellers_before,
-  b.sellers_after,
-  b.ratiopricediff,
-  b.dayspast
-  FROM map_pluto a, flips b
-  WHERE a.borocode = b.borough 
-    AND a.block = b.block 
-    AND a.lot = b.lot;
+GROUP BY bbl;
