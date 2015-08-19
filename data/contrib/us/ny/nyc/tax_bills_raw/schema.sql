55435d18b0f3fce01e11e06f0603f481ae032bd7
John Krauss <irving.krauss@gmail.com>
2015-08-19 12:20:26 -0400

    adding rawdata taxbills

---
+++ data/contrib/us/ny/nyc/tax_bills_raw/schema.sql
@@ -0,0 +1,11 @@
+create table tax_bills_raw (
+  bbl BIGINT,
+  activityThrough DATE,
+  section TEXT,
+  key TEXT,
+  dueDate DATE,
+  activityDate DATE,
+  value TEXT,
+  meta TEXT,
+  apts TEXT
+);
